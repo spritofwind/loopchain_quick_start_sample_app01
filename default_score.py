0c9093b2cdadc7aff2a0344f56000661869e893a
JinhoYoo <jinho.yoo@nomadconnection.com>
2017-07-27 18:21:51 +0900

    Added comment.

--- default_score.py
+++ default_score.py
@@ -3,8 +3,7 @@ from loopchain.blockchain import ScoreBase
 
 
 class UserScore(ScoreBase):
-    """ Basic User SCORE file.
-
+    """ Basic User SCORE code.
     Keep 'UserScore' name.
     """
     def invoke(self, transaction, block):
@@ -12,19 +11,33 @@ class UserScore(ScoreBase):
 
         @param trasaction loopchain.blockchain.Transaction class.
         @param block  loopchain.blockchain.Block class.
-        @return return Score result.
+        @return Score result. It's dictionary with 'code' and 'message' like below.
+            {
+              'code' : integer value.
+                * Success is 0.
+                * Not found is 2.
+                * Exception is 9000.
+              'message' : 'error_message when code not success'
+            }
         """
 
         pass
 
     def query(self, params):
+        """ Return the result by invoke().
+
+        @param params in dictionary from query request.
+        @return Score result. Convert Dictionary type as JSON string.
         """
-        @params 
-        @return
-        """
+
 
         return params
 
     def info(self):
+        """ Return the result by invoke().
+
+        @param params in dictionary from query request.
+        @return Score result. Convert Dictionary type as JSON string.
+        """
 
         return None
